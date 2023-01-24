399c8efce365b169d5936951b0d9013ebea8056f
bcc02 <roberta.lorenzi.93@gmail.com>
2023-01-24 14:43:32 +0100

    update of GCM builder routine

--- run_GCM_builder_and_estim.m
+++ run_GCM_builder_and_estim.m
@@ -1,14 +1,30 @@
 % 
 clc
 clear
+%% ============ DCM specify ===============================================
+%protDir = '/media/bcc/Volume/Analysis/Roberta/DCM/AE_P5/';
+wDir= pwd;
 
-protDir = '/media/bcc/Volume/Analysis/Roberta/DCM/AE_P5/';
-GCM_name = 'GCM_modRL_BF.mat';
+protDir = '/media/bcc/Volume/Roberta/DCM/AE_P5/';
+
+init_subj = 1;
+nsubj = 23;
+
+DCM_filename = 'pp';
+
+estimate_bool = false; 
+
+trial_visuomotor_DCM(protDir, init_subj, nsubj, DCM_filename, estimate_bool)
+
+cd(wDir)
+
+%% ============ GCM builder and estimate ==================================
+GCM_name = 'rrtf';
 init_subj = 1;
 nsubj = 23;
 
 %DCM_name = {'^DCM_2022823_h09m46+'};
-DCM_name={'^DCM_20221011_+'};
+DCM_name={'_pp+'};
 
 GCM = GCM_builder(protDir,fullfile(protDir,'GCM_models'), GCM_name, ...
     DCM_name, init_subj, nsubj);
